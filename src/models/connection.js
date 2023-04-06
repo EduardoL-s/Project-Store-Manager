f16b7eab08e33d4b19f8e9620be3f0b9365ecf84
EduardoL-s <eduardoluizmm@hotmail.com>
2023-04-06 02:10:11 -0300

    correção da connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
-  port: 3306,
-  host: 'db',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
